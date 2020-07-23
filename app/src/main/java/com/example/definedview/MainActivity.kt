2380703e79643eec0fa187e72f202e6b131e7577
kaikai951206 <sky19960125>
2020-07-23 16:46:59 +0800

    flowLayout

--- app/src/main/java/com/example/definedview/MainActivity.kt
+++ app/src/main/java/com/example/definedview/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.definedview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.example.myapplication.FlowLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FlowLayout.OnItemClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        flowLayout.setOnItemClickListener(this)
     }
+
+    override fun onItemClick(view: View) {
+        when (view) {
+            is TextView -> flowLayout.removeView(view)
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/definedview/view/FlowLayout.kt
+++ app/src/main/java/com/example/definedview/view/FlowLayout.kt
@@ -1,148 +1,103 @@
-package com.example.definedview.view
+package com.example.myapplication
 
 import android.content.Context
-import android.content.res.Resources
 import android.util.AttributeSet
-import android.util.Log
-import android.util.TypedValue
 import android.view.View
 import android.view.ViewGroup
 import kotlin.math.max
 
-class FlowLayout @JvmOverloads constructor(context: Context, attributeSet: AttributeSet) : ViewGroup(context, attributeSet) {
-    companion object {
-        const val TAG = "FlowLayout"
-    }
-
-    private val mHorizontalSpacing = dp2px(16)
-    private val mVerticalSpacing = dp2px(8)
-
-    //记录所有的行，一行一行的存储
-    private var allLines = ArrayList<List<View>>()
-
-    //记录每一行的行高
-    private var lineHeights = ArrayList<Int>()
-
-    private fun dp2px(dp: Int): Int {
-        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(), Resources.getSystem().displayMetrics).toInt()
-    }
-
-    private fun initMeasureParams() {
-        allLines = ArrayList<List<View>>()
-        lineHeights = ArrayList<Int>()
-    }
+class FlowLayout @JvmOverloads constructor(context: Context, attributeSet: AttributeSet) :
+    ViewGroup(context, attributeSet) {
+    private val mHorizontalSpace = 10
+    private val mVerticalSpace = 10
+    private var mOnItemClickListener: OnItemClickListener? = null
 
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        initMeasureParams()
-        //度量子view的大小,度量所有子View
+        val measuredWidthSize = MeasureSpec.getSize(widthMeasureSpec)
+        val measuredHeightSize = MeasureSpec.getSize(heightMeasureSpec)
+        val measuredWidthMode = MeasureSpec.getMode(widthMeasureSpec)
+        val measuredHeightMode = MeasureSpec.getMode(heightMeasureSpec)
 
-        val selfWidth = MeasureSpec.getSize(widthMeasureSpec)//viewgroup解析的宽度
-        val selfHeight = MeasureSpec.getSize(heightMeasureSpec)//viewgroup解析的高度
-
-        var lineViews = ArrayList<View>()//保存一行中所有的view
-        var lineWidthUsed = 0 //记录这行已经使用了多宽的size
-        var lineHeight = 0 //一行的行高
-
-        var parentNeededWidth = 0 //measure过程中，子view要求的父viewGroup的宽
-        var parentNeededHeight = 0 //measure过程中，子view要求的父viewGroup的高
+        var realWidth = 0
+        var realHeight = 0
+        var lineWidth = 0
+        var lineHeight = 0
 
         for (i in 0 until childCount) {
             val childView = getChildAt(i)
+            measureChild(childView, widthMeasureSpec, heightMeasureSpec)
+            val childWidth = childView.measuredWidth + mHorizontalSpace
+            val childHeight = childView.measuredHeight + mVerticalSpace
 
-            val childLP = childView.layoutParams
-            val childWidthMeasureSpec = getChildMeasureSpec(widthMeasureSpec, paddingLeft + paddingRight, childLP.width)
-            val childHeightMeasureSpec = getChildMeasureSpec(heightMeasureSpec, paddingTop + paddingBottom, childLP.height)
+            if (lineWidth + childWidth > measuredWidthSize) {
+                //换行
+                realWidth = max(lineWidth, childWidth)
+                realHeight += lineHeight
 
-            childView.measure(childWidthMeasureSpec, childHeightMeasureSpec)
+                lineWidth = childWidth
+                lineHeight = childHeight
 
-            //获取子view 的宽高
-            val childMeasuredWidth = childView.measuredWidth
-            val childMeasuredHeight = childView.measuredHeight
-
-            //通过宽度来判断是否需要换行，通过换行后的每行的行高来获取整个viewGroup的行高
-            //如果需要换行
-            if (childMeasuredWidth + lineWidthUsed + mHorizontalSpacing > selfWidth) {
-                allLines.add(lineViews)
-                lineHeights.add(lineHeight)
-                //一旦换行，我们就可以判断当前行需要的宽和高了，所以此时要记录下来
-                parentNeededHeight += lineHeight + mVerticalSpacing
-                parentNeededWidth = max(parentNeededWidth, lineWidthUsed + mHorizontalSpacing)
-
-                lineViews = ArrayList<View>()
-                lineWidthUsed = 0
-                lineHeight = 0
-            }
-            // view 是分行layout的，所以要记录每一行有哪些view，这样可以方便layout布局
-            lineViews.add(childView)
-            //每行都会有自己的宽和高
-            lineWidthUsed += childMeasuredWidth + mHorizontalSpacing
-            lineHeight = max(lineHeight, childMeasuredHeight)
-
-            //如果当前childView是最后一行的最后一个
-
-            //如果当前childView是最后一行的最后一个
-            if (i == childCount - 1) { //最后一行
-                lineHeights.add(lineHeight)
-                allLines.add(lineViews)
-                parentNeededWidth = Math.max(parentNeededWidth, lineWidthUsed)
-                parentNeededHeight += lineHeight
+            } else {
+                lineWidth += childWidth
+                lineHeight = max(lineHeight, childHeight)
             }
 
+            //统计最后一行的宽度和高度
+            if (i == childCount - 1) {
+                realWidth = max(realWidth, childWidth)
+                realHeight += lineHeight
+            }
         }
 
-        //根据子View的度量结果，来重新度量自己ViewGroup
-        // 作为一个ViewGroup，它自己也是一个View,它的大小也需要根据它的父亲给它提供的宽高来度量
-        val widthMode = MeasureSpec.getMode(widthMeasureSpec)
-        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+        setMeasuredDimension(
+            if (measuredWidthMode == MeasureSpec.EXACTLY) measuredWidthSize else realWidth,
+            if (measuredHeightMode == MeasureSpec.EXACTLY) measuredHeightSize else realHeight
+        )
 
-        val realWidth = if (widthMode == MeasureSpec.EXACTLY) selfWidth else parentNeededWidth
-        val realHeight = if (heightMode == MeasureSpec.EXACTLY) selfHeight else parentNeededHeight
-
-        setMeasuredDimension(realWidth, realHeight)
 
     }
 
 
     override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
-
-        val lineCount = allLines.size
-        var curL = 0
-        Log.d(TAG, "onLayout: getLeft(): $left")
-        var curT = 0
-        Log.d(TAG, "onLayout: getTop(): $top")
-        for (i in 0 until lineCount) {
-            val lineViews = allLines[i]
-            val lineHeight = lineHeights[i]
-            for (j in lineViews.indices) {
-                val view = lineViews[j]
-                val left = curL
-                val top = curT
-                val bottom = top + view.measuredHeight
-                val right = left + view.measuredWidth
-                view.layout(left, top, right, bottom)
-                curL = right + mHorizontalSpacing
+        var lineWidth = 0
+        var lineHeight = 0
+        var left = 0
+        var top = 0
+        for (i in 0 until childCount) {
+            val childView = getChildAt(i)
+            childView.setOnClickListener {
+                mOnItemClickListener?.onItemClick(childView)
             }
-            curL = 0
-            curT += lineHeight + mVerticalSpacing
+            val childWidth = childView.measuredWidth
+            val childHeight = childView.measuredHeight
+
+            if (lineWidth + childWidth + mHorizontalSpace > measuredWidth) {
+                top += lineHeight + mVerticalSpace
+                left = 0
+
+                lineWidth = childWidth
+                lineHeight = childHeight
+            } else {
+                lineWidth += childWidth
+                lineHeight = max(lineHeight, childHeight)
+            }
+
+            childView.layout(left, top, left + childWidth, top + childHeight)
+            left += childWidth + mHorizontalSpace
+
         }
-
     }
+
+    interface OnItemClickListener {
+        fun onItemClick(view: View)
+    }
+
+
+    fun setOnItemClickListener(onItemClickListener: OnItemClickListener) {
+        mOnItemClickListener = onItemClickListener
+    }
+
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
